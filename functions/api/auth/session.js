32b417f8d5a7699fcb47a5f9cabf75ea3b749935
Brucker <bigpapadanger@gmail.com>
2026-08-24 20:02:53 -0400

    cleaned one session function

--- functions/api/auth/session.js
+++ functions/api/auth/session.js
@@ -1,5 +1,7 @@
+/*
 import { handleAuthSession } from "../../services/rocketleague/session.js";
 
 export async function onRequest(context) {
   return handleAuthSession(context.request, context.env);
 }
+*/
