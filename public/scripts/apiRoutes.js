4de9eb60e97d1b07da5b07906c4d38e69a004de5
Brucker <bigpapadanger@gmail.com>
2026-08-25 18:05:10 -0400

    reroute

--- public/scripts/apiRoutes.js
+++ public/scripts/apiRoutes.js
@@ -8,5 +8,5 @@ export const OCR_TRACKING_URL="/api/ocr/localTracking"
 export const ROCKET_LEAGUE_SESSION_URL = "/api/auth/rocketleague/session";
 export const ROCKET_LEAGUE_PROFILE_URL = "/api/auth/rocketleague/profile";
 export const BPD_AUTH_SESSION_URL = "/api/auth/session";
-export const BPD_AUTH_LOGOUT_URL = "/api/auth/logout";
+export const BPD_AUTH_LOGOUT_URL = "/api/auth/epic/epic_logout";
 export const EPIC_LOGIN_URL = "/api/auth/epic/epic_login";
