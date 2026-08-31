16031dd8fb131a6a148ebff98da199988881c22c
Brucker <bigpapadanger@gmail.com>
2026-08-31 13:55:09 -0400

    updated functions folder

--- functions/api/auth/session.js
+++
@@ -1,7 +0,0 @@
-/*
-import { handleAuthSession } from "../../services/rocketleague/session.js";
-
-export async function onRequest(context) {
-  return handleAuthSession(context.request, context.env);
-}
-*/
